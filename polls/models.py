1db7d70663966ba9dc97ad51ff94b1ce728336bd
Eleonore Hasler <eleonore@hasler.fr>
2026-08-19 17:03:18 +0200

    add option to group recent questions

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,23 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # My models
+
+# To create instances of models run "python models.py shell"
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+
+    # Display question_text when checking Question.objects.all()
     def __str__(self):
         return self.question_text
+
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+    
 
 
 class Choice(models.Model):
