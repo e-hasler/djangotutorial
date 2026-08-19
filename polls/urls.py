eccfbc45959d4c20a8f40ce53e547b616d1e31ec
Eleonore Hasler <eleonore@hasler.fr>
2026-08-20 00:45:39 +0200

    removing harcoded urls in templates

--- polls/urls.py
+++ polls/urls.py
@@ -7,7 +7,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:question_id>", views.detail, name="detail"),
+    path("specifics/<int:question_id>", views.detail, name="detail"),
     path("<int:question_id>/result", views.results, name="results"), 
     # Why only views.results since it is associated to a question (that is not given here)?
     # How does it know the results of which question to display?
