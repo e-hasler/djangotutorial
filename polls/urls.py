76a07638ae591ba6492c19c4e271e1aed1aba7c9
Eleonore Hasler <eleonore@hasler.fr>
2026-08-20 19:59:41 +0200

    use generic templates in views

--- polls/urls.py
+++ polls/urls.py
@@ -9,11 +9,8 @@ from . import views
 
 app_name = "polls" # Add if several apps in the project
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("specifics/<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/result/", views.results, name="results"), 
-    # Why only views.results since it is associated to a question (that is not given here)?
-    # How does it know the results of which question to display?
-    path("<int:question_id>/vote/", views.vote, name = "vote"),
-    # Same question here
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -7,32 +7,25 @@ from django.db.models import F
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
-from .models import Question
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context) # We provide the template path and the context
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
-    """
-    return render(request, "polls/index.html", context)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
-    is a shortcut for 
-
-    template = loader.get_template("polls/index.html")
-    return HttpResponse(template.render(context, request))
-    """
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
