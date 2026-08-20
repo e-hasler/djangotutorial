8eebdd4593cb54d45dd38abd27a66b65e0cd55ba
Eleonore Hasler <eleonore@hasler.fr>
2026-08-21 00:38:49 +0200

    add choices and admin questions

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,26 @@
+"""
+polls/admins.py
+
+Register your models here.
+"""
+
 from django.contrib import admin
-
-# Register your models here.
-
 from .models import Question, Choice
 
-admin.site.register(Question)
+# Admin classes
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # choose order of fields to display with fields = ["field", ...]
+    # and split in sets using fieldsets = [("DesiredFieldName", {"fields": ["field"]}), ...]
+    fieldsets = [
+        ("Da question", {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+
+# Register all models admins want to control
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
