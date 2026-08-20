d387d8dda0c7700503d28df84c8804d7b4b9f3d3
Eleonore Hasler <eleonore@hasler.fr>
2026-08-20 20:10:28 +0200

    add test and fix published_recently

--- polls/models.py
+++ polls/models.py
@@ -20,7 +20,7 @@ class Question(models.Model):
 
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+        return timezone.now() >= self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
     
 
 
