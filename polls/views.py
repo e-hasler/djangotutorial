446a2e33badae8d1047c7c3a975d6d6850861104
Eleonore Hasler <eleonore@hasler.fr>
2026-08-20 19:47:40 +0200

    update vote number

--- polls/views.py
+++ polls/views.py
@@ -3,11 +3,12 @@ views.py
 The views are the different types of pages
 """
 
-from django.http import HttpResponse
-from django.http import Http404
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.template import loader
+from django.urls import reverse
 
+from .models import Choice, Question
 
 from .models import Question
 
@@ -33,4 +34,23 @@ def results(request, question_id):
     return HttpResponse("You are looking at the results of question %s" % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You are on the voting page of question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # redisplay the question voting form 
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
